d891dd04529e9d8c943800f5db86302c4a59626c
Shanmuga S Nuthalapati <ssnuthalapati@gmail.com>
2018-05-23 16:47:28 -0700

    Replicating the same PythonWebApp to demonstrate the SSO

---
+++ config.py
@@ -0,0 +1,8 @@
+RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
+TENANT = "TMOAAD.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
+CLIENT_ID = "502b4f50-a9fb-4522-a351-3940afe6bd1f"  # copy the Application ID of your app from your Azure portal
+CLIENT_SECRET = "fvzXNVcAjI2KYblnMVmFinA6NzE8ozWl2eHgwWtREt8="  # copy the value of key you generated when setting up the application
+
+# These settings are for the Microsoft Graph API Call
+API_VERSION = 'v1.0'
